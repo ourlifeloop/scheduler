8bdcef1a9785d9d16db1b8207edfb3a889af5657
Mitchel Pigsley <mpigsley@gmail.com>
2019-05-08 17:43:50 -0500

    Connect to discord via bot and send message at 9a

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Discord Scheduler
 
-todo
+Bot Invite
+https://discordapp.com/api/oauth2/authorize?client_id=CLIENT_ID&scope=bot&permissions=1
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const Discord = require('discord.js');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const client = new Discord.Client();
+admin.initializeApp();
+
+exports.notifyDiscord = functions.pubsub
+  .schedule('every day 09:00')
+  .timeZone('America/Chicago')
+  .onRun(async () => {
+    const { token, channel } = functions.config().discord;
+    await client.login(token);
+    console.log(`Logged in as ${client.user.tag}`);
+
+    const textChannel = client.channels.find(
+      obj => obj.name === channel && obj.type === 'text',
+    );
+    if (!textChannel) {
+      throw new Error(`Text channel not found by the name "${channel}"`);
+    }
+    // await textChannel.send("Bleep Blorp, I'm a robot");
+  });
