714cc0247377ccacf1501e90f69749a7262b417c
Mitchel Pigsley <mpigsley@gmail.com>
2021-09-20 13:32:05 -0600

    Add a site loader

--- src/components/protected-routes/protected-routes.js
+++ src/components/protected-routes/protected-routes.js
@@ -3,9 +3,13 @@ import { Router } from '@reach/router';
 import PropTypes from 'prop-types';
 
 import OnCallScheduler from 'components/on-call-scheduler';
+import FlexContainer from 'primitives/flex-container';
 import Navigation from 'components/navigation';
 import { usePrevious } from 'utils/effects';
 import Calendar from 'components/calendar';
+import Spinner from 'primitives/spinner';
+
+import styles from './styles.module.scss';
 
 export default function ProtectedRoutes({ isLoggedIn, isInitialized, toHome }) {
   const prevIsLoggedIn = usePrevious(isLoggedIn);
@@ -21,7 +25,13 @@ export default function ProtectedRoutes({ isLoggedIn, isInitialized, toHome }) {
   });
 
   if (!isInitialized) {
-    return null;
+    return (
+      <div className="container">
+        <FlexContainer justify="center" className={styles.loader}>
+          <Spinner size={60} />
+        </FlexContainer>
+      </div>
+    );
   }
 
   return (
